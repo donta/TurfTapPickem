43c61f79382ecccb9edf9eaa0174ba6b86c15262
Donta Dalpoas <donta.j.dalpoas.ctr@army.mil>
2025-07-29 15:49:09 -0400

    trying to match example

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,14 +1,22 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 const schema = a.schema({
-	Todo: a.model({
-		content: a.string(),
-	})
-	.authorization((allow: { public: () => any; }) => [allow.public()]),
+  Todo: a
+    .model({
+      content: a.string(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
-	schema,
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "apiKey",
+    // API Key is used for a.allow.public() rules
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
+  },
 });
