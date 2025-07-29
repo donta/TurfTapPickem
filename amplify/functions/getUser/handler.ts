13b65473809bdc39b2a07a1f8d7babe36b4d418e
Donta Dalpoas <donta.j.dalpoas.ctr@army.mil>
2025-07-29 11:23:50 -0400

    fixing handler error

--- amplify/functions/getUser/handler.ts
+++ amplify/functions/getUser/handler.ts
@@ -1,3 +1,17 @@
-export const handler = async (event) => {
-	return `Hello from my first function!${event.body}`;
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+	return {
+		statusCode: 200,
+		headers: {
+			"Content-Type": "application/json",
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Headers": "Content-Type",
+			"Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+		},
+		body: JSON.stringify({
+			message: "Hello from getUserFunction!",
+			event: event.body || "No body provided",
+		}),
+	};
 };
